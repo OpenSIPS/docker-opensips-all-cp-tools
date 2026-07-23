662252f8e3a557ccd1ceeae9606d147973babca8
Stefan Darius <darius.stefan@opensips.org>
2026-07-23 11:09:53 +0200

    mysql: change to db schema from packages

---
+++ etc/mysql/00-dbschema.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+echo "Installing OpenSIPS database schema..."
+
+cd "$(dirname "$0")"
+. functions
+
+TABLE_FILES=(
+    acc-create.sql
+    alias_db-create.sql
+    auth_db-create.sql
+    authjwt-create.sql
+    b2b-create.sql
+    b2b_sca-create.sql
+    cachedb_sql-create.sql
+    call_center-create.sql
+    carrierroute-create.sql
+    closeddial-create.sql
+    clusterer-create.sql
+    cpl-create.sql
+    db-migrate.sql
+    dialog-create.sql
+    dialplan-create.sql
+    dispatcher-create.sql
+    domain-create.sql
+    domainpolicy-create.sql
+    drouting-create.sql
+    emergency-create.sql
+    fraud_detection-create.sql
+    freeswitch_scripting-create.sql
+    group-create.sql
+    imc-create.sql
+    load_balancer-create.sql
+    msilo-create.sql
+    permissions-create.sql
+    presence-create.sql
+    qrouting-create.sql
+    ratecacher-create.sql
+    registrant-create.sql
+    rls-create.sql
+    rtpengine-create.sql
+    rtpproxy-create.sql
+    smpp-create.sql
+    speeddial-create.sql
+    sqlops-create.sql
+    table-migrate.sql
+    tcp_mgm-create.sql
+    tls_mgm-create.sql
+    tracer-create.sql
+    userblacklist-create.sql
+    usrloc-create.sql
+)
+
+add_table /usr/share/opensips/mysql/standard-create.sql
+
+for file in "${TABLE_FILES[@]}"; do
+    add_table /usr/share/opensips/mysql/$file
+done
--- etc/opensips-cp/docker-entrypoint.d/db_default.sh
+++ etc/opensips-cp/docker-entrypoint.d/db_default.sh
@@ -22,3 +22,10 @@ SELECT '${DEFAULT_DOMAIN}'
 WHERE NOT EXISTS (
     SELECT 1 FROM domain WHERE domain = '${DEFAULT_DOMAIN}'
 );"
+
+mysql -h ${MYSQL_IP} -u ${MYSQL_USER} -p"${MYSQL_PASSWORD}" -D ${MYSQL_DATABASE} -e "
+INSERT INTO clusterer (cluster_id, node_id, url, state, no_ping_retries, priority, sip_addr, flags, description)
+SELECT 1, 1, 'bin:${OPENSIPS_IP}:5566', 1, 3, 50, '${OPENSIPS_IP}:5060', '', 'OpenSIPS CP Clusterer Node 1'
+WHERE NOT EXISTS (
+    SELECT 1 FROM clusterer WHERE cluster_id = 1 AND node_id = 1
+);"
